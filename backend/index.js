6872e649f75e8abb39d1fd91974523249fd09ccf
Nasah-Kuma <nasahnash19@gmail.com>
2026-07-22 15:52:57 +0100

    created a local dabatase and connected to it

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./connection.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+connectDB().then(() => {
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server running on ${process.env.PORT || 3000}`);
+  });
+});
